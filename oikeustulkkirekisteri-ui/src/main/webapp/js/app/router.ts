726d4bf1a72e17479317147114f2183c47c0ff94
Risto Autio <risto.autio@dicode.fi>
2016-06-01 16:07:07 +0300

    lisätty angular router. Navigointi uusittu. Templatet lisätty.

---
+++ oikeustulkkirekisteri-ui/src/main/webapp/js/app/router.ts
@@ -0,0 +1,23 @@
+angular.module('myApp').config(($routeProvider) => {
+  $routeProvider
+
+      .when('/search', {
+        templateUrl: 'templates/translatorSearch.html',
+        controller: 'translatorSearchCtrl'
+      })
+
+      .when('/addTranslator', {
+        templateUrl: 'templates/translatorCreate.html',
+        controller: 'translatorCreateCtrl'
+      })
+
+      .when('/translator/:id', {
+        templateUrl: 'templates/translatorView.html',
+        controller: 'translatorViewCtrl'
+      })
+
+      .otherwise({
+        redirect: '/search'
+      });
+});
+
--- oikeustulkkirekisteri-ui/src/main/webapp/js/entry.js
+++ oikeustulkkirekisteri-ui/src/main/webapp/js/entry.js
@@ -1,2 +1,4 @@
 require('./app/app.js');
+require('./app/app.ts');
+require('./app/router.ts');
 require("../css/main.scss");
