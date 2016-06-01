37417067d13ae4a9461d99778cb5393729a28a22
Risto Autio <risto.autio@dicode.fi>
2016-06-01 09:09:50 +0300

    muokattu henkilön tarkastelu näkymää. tyylejä korjailtu.

--- oikeustulkkirekisteri-ui/src/main/webapp/webpack.config.js
+++ oikeustulkkirekisteri-ui/src/main/webapp/webpack.config.js
@@ -15,12 +15,10 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style-loader!css-loader" },
-            { test: /\.scss$/, loaders: ["style", "css", "sass"] },
-            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
             { test: /\.css$/, loader: 'style!css' },
             { test: /\.ts$/, loader: 'typescript-loader' },
-            { test: /\.scss$/, loader: "style!css!sass"}
+            { test: /\.scss$/, loader: "style!css!sass"},
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
         ]
     },
     plugins: [ 
