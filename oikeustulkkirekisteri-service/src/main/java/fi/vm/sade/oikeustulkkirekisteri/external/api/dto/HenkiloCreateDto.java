a09419a6672ca21f0224931e43e6fa4c978a32aa
Tommi Ratamaa <tommi.ratamaa@gmail.com>
2016-06-03 09:56:00 +0300

    OS X letter case rename retry (git mv -f)

---
+++ oikeustulkkirekisteri-service/src/main/java/fi/vm/sade/oikeustulkkirekisteri/external/api/dto/HenkiloCreateDto.java
@@ -0,0 +1,29 @@
+package fi.vm.sade.oikeustulkkirekisteri.external.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * User: tommiratamaa
+ * Date: 30.5.2016
+ * Time: 14.50
+ */
+@Getter @Setter
+public class HenkiloCreateDto_ implements Serializable {
+    private String etunimet;
+    private String kutsumanimi;
+    private String sukunimi;
+    private String hetu;
+    private Date syntymaaika;
+    private String oidHenkilo;
+    private String sukupuoli;
+    private KielisyysDto asiointiKieli;
+    private KansalaisuusDto kansalaisuus;
+    private KielisyysDto aidinkieli;
+    private List<OrganisaatioHenkiloDto_> organisaatioHenkilo = new ArrayList<>();
+}
