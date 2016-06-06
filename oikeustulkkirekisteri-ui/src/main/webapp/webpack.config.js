e464431bee2f56ffbb6bc74199e5dfd1c321f130
Risto Autio <risto.autio@dicode.fi>
2016-06-06 15:20:35 +0300

    korjaus select2:een

--- oikeustulkkirekisteri-ui/src/main/webapp/webpack.config.js
+++ oikeustulkkirekisteri-ui/src/main/webapp/webpack.config.js
@@ -5,7 +5,8 @@ var path = require('path'),
 module.exports = {
     entry: {
         'app.min': ['./js/entry.js'],
-        'lib.min': ['./js/lib.js']
+        'lib.min': ['./js/lib.js'],
+        'select.min': ['./js/select.js']
     },
     devtool: 'source-map',
     output: {
