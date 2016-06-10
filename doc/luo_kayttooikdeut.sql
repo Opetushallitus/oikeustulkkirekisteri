3ebfcf9a0ce88ac6020abaef7326b94d11bbae3d
Tommi Ratamaa <tommi.ratamaa@gmail.com>
2016-06-10 21:25:05 +0300

    - Lisätty authenticationin kantaan tehty CRUD-käyttöoikeus (SQL dokumentaatioksi) - Lisätty palvelukäyttäjätunnukset authentication-serivceen

---
+++ doc/luo_kayttooikdeut.sql
@@ -0,0 +1,27 @@
+-- authentication-kantaan:
+
+begin;
+
+insert into text_group(id, version) values ((select nextval('hibernate_sequence')*10), 0);
+insert into text(id, version, lang, text, textgroup_id) values ((select nextval('hibernate_sequence')*10), 0,
+                                                                'FI', 'Oikeustulkkirekisteri', (select max(id) from text_group));
+insert into text(id, version, lang, text, textgroup_id) values ((select nextval('hibernate_sequence')*10), 0,
+                                                                'SV', 'Oikeustulkkirekisteri', (select max(id) from text_group));
+insert into text(id, version, lang, text, textgroup_id) values ((select nextval('hibernate_sequence')*10), 0,
+                                                                'EN', 'Oikeustulkkirekisteri', (select max(id) from text_group));
+insert into palvelu (id, version, name, palvelutyyppi, textgroup_id)
+values ((select nextval('hibernate_sequence')*10), 0, 'OIKEUSTULKKIREKISTERI', 'YKSITTAINEN', (select max(id) from text_group));
+
+
+insert into text_group(id, version) values ((select nextval('hibernate_sequence')*10), 0);
+insert into text(id, version, lang, text, textgroup_id) values ((select nextval('hibernate_sequence')*10), 0,
+                                                                'FI', 'Oikeustulkkirekisterin ylläpito', (select max(id) from text_group));
+insert into text(id, version, lang, text, textgroup_id) values ((select nextval('hibernate_sequence')*10), 0,
+                                                                'SV', 'Oikeustulkkirekisterin ylläpito', (select max(id) from text_group));
+insert into text(id, version, lang, text, textgroup_id) values ((select nextval('hibernate_sequence')*10), 0,
+                                                                'EN', 'Oikeustulkkirekisterin ylläpito', (select max(id) from text_group));
+insert into kayttooikeus(id, version, palvelu_id, rooli, textgroup_id)
+values ((select nextval('hibernate_sequence')*10), 0, (select id from palvelu where name = 'OIKEUSTULKKIREKISTERI'),
+        'OIKEUSTULKKIREKISTERI_CRUD', (select max(id) from text_group));
+
+commit;
