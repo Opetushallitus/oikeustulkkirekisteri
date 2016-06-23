f1bc4a9cc2b24e626a703eb341f1970b6281a24f
Risto Autio <risto.autio@dicode.fi>
2016-06-23 13:52:35 +0300

    pakotetaan headerin lataaminen jos kieli vaihdetaan

--- oikeustulkkirekisteri-ui/src/main/webapp/js/app/controller/commonControllers.ts
+++ oikeustulkkirekisteri-ui/src/main/webapp/js/app/controller/commonControllers.ts
@@ -1,14 +1,26 @@
 angular.module('registryApp').controller('mainCtrl', ["$scope", "Page", "LocalisationService", "$route",
   ($scope, Page, LocalisationService, $route) => {
+
     $scope.Page = Page;
-
     $scope.currentLocale = LocalisationService.getLocale().toUpperCase();
+    $scope.showHeader = true;
 
+    //ng-include wont update localization otherwise
+    const forceHeaderUpdate = () => {
+      $scope.showHeader = false;
+      $route.reload();
+      setTimeout(()=> {
+        $scope.showHeader = true;
+        $route.reload();
+      }, 1);
+    };
+    
     $scope.switchLanguage = () => {
       const toLocale = LocalisationService.getLocale() === 'fi' ? 'sv' : 'fi';
       LocalisationService.setLocale(toLocale);
       $scope.currentLocale = LocalisationService.getLocale().toUpperCase();
       $route.reload();
+      forceHeaderUpdate();
     };
 
   }]);
