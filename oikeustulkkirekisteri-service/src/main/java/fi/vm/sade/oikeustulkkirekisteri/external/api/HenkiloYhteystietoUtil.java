3045106c05d93b74ce4442f928af8c60067a3dde
Jarkko Pätäri <jarkko.patari@gofore.com>
2017-01-12 09:21:37 +0200

    KJHH-942: Näytetään ensisijaisesti vtj:n yhteystiedot

--- oikeustulkkirekisteri-service/src/main/java/fi/vm/sade/oikeustulkkirekisteri/external/api/HenkiloYhteystietoUtil.java
+++ oikeustulkkirekisteri-service/src/main/java/fi/vm/sade/oikeustulkkirekisteri/external/api/HenkiloYhteystietoUtil.java
@@ -5,6 +5,7 @@ import fi.vm.sade.oikeustulkkirekisteri.external.api.dto.HenkiloRestDto;
 import fi.vm.sade.oikeustulkkirekisteri.external.api.dto.YhteystiedotDto;
 import fi.vm.sade.oikeustulkkirekisteri.external.api.dto.YhteystiedotRyhmaDto;
 import fi.vm.sade.oikeustulkkirekisteri.util.CustomOrderComparator;
+import java.util.Comparator;
 
 import java.util.Optional;
 import java.util.function.Predicate;
@@ -22,13 +23,16 @@ import static java.util.Comparator.nullsLast;
 public class HenkiloYhteystietoUtil {
     public static final String KOTIOSOITE_TYYPPI = "yhteystietotyyppi1";
     public static final String TYOOSOITE_TYYPPI = "yhteystietotyyppi2";
+    public static final String VAKINAINEN_KOTIMAAN_OSOITE_TYYPPI = "yhteystietotyyppi4";
+    public static final String VAKINAINEN_ULKOMAAN_OSOITE_TYYPPI = "yhteystietotyyppi5";
     public static final Predicate<YhteystiedotRyhmaDto> YT_RYHMA_FILTER_READ = r -> !r.isRemoved() && !r.getRyhmaKuvaus().equals(KOTIOSOITE_TYYPPI);
     public static final Predicate<YhteystiedotRyhmaDto> YT_RYHMA_FILTER_SET = YT_RYHMA_FILTER_READ.and(r -> !r.isReadOnly());
     private HenkiloYhteystietoUtil() {
     }
 
     public static Stream<YhteystiedotDto> findYhteystieto(HenkiloRestDto henkilo, Predicate<YhteystiedotRyhmaDto> predicate, YhteystietoTyyppi tyyppi) {
-        return henkilo.getYhteystiedotRyhma().stream().sorted(comparing(YhteystiedotRyhmaDto::getRyhmaKuvaus, nullsLast(new CustomOrderComparator<>(TYOOSOITE_TYYPPI).thenComparing(naturalOrder()))))
+        Comparator<String> comparator = new CustomOrderComparator<>(VAKINAINEN_KOTIMAAN_OSOITE_TYYPPI, VAKINAINEN_ULKOMAAN_OSOITE_TYYPPI, TYOOSOITE_TYYPPI);
+        return henkilo.getYhteystiedotRyhma().stream().sorted(comparing(YhteystiedotRyhmaDto::getRyhmaKuvaus, nullsLast(comparator.thenComparing(naturalOrder()))))
                 .filter(predicate).flatMap(yt -> yt.getYhteystiedot().stream())
                 .filter(yt -> yt.getYhteystietoTyyppi() == tyyppi)
                 .filter(yt -> yt.getYhteystietoArvo() != null && !yt.getYhteystietoArvo().isEmpty());
