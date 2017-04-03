03590ef7f96b18eed49eb5ec2cf932313f108494
Jarkko Pätäri <jarkko.patari@gofore.com>
2017-04-03 10:33:39 +0300

    KJHH-970: Port to oppijanumerorekisteri

--- oikeustulkkirekisteri-service/src/main/java/fi/vm/sade/oikeustulkkirekisteri/external/api/HenkiloApi.java
+++ oikeustulkkirekisteri-service/src/main/java/fi/vm/sade/oikeustulkkirekisteri/external/api/HenkiloApi.java
@@ -3,10 +3,8 @@ package fi.vm.sade.oikeustulkkirekisteri.external.api;
 import com.wordnik.swagger.annotations.ApiParam;
 import fi.vm.sade.oikeustulkkirekisteri.external.api.dto.HenkiloCreateDto;
 import fi.vm.sade.oikeustulkkirekisteri.external.api.dto.HenkiloRestDto;
-import fi.vm.sade.oikeustulkkirekisteri.external.api.dto.PaginationObject;
 
 import javax.ws.rs.*;
-import java.util.List;
 
 import static fi.vm.sade.oikeustulkkirekisteri.external.api.Constants.JSON;
 
@@ -14,26 +12,12 @@ import static fi.vm.sade.oikeustulkkirekisteri.external.api.Constants.JSON;
  * User: tommiratamaa
  * Date: 30.5.2016
  * Time: 14.50
+ *
+ * @deprecated {@link OppijanumerorekisteriApi}
  */
 @Path("/resources/henkilo")
+@Deprecated
 public interface HenkiloApi {
-    @GET
-    @Produces(JSON)
-    PaginationObject<HenkiloRestDto> listHenkilos(
-            @ApiParam("Hakuparametri, henkilötunnus, nimi tai henkilö-oid") @QueryParam("q") String queryParam,
-            @ApiParam("Henkilötyypin määre") @QueryParam("ht") String henkiloTyyppi,
-            @ApiParam("Haun määrärajoite, arvo 0 tarkoittaa hae kaikki") @QueryParam("count") Integer count,
-            @ApiParam("Haun aloitusindeksi") @QueryParam("index") Integer index,
-            @ApiParam("Rajaus organisaatioiden mukaan") @QueryParam("org") List<String> orgOid,
-            @ApiParam("Rajaus palvelun mukaan") @QueryParam("serviceName") String serviceName,
-            @ApiParam("Käyttöoikeusryhmärajaus") @QueryParam("groupName") String groupName,
-            @ApiParam("Järjestyksen määrääminen") @QueryParam("order") String order,
-            @ApiParam(value= "Myös passiivisten henkilöiden haku", defaultValue="false") @QueryParam("p") Boolean passives,
-            @ApiParam("Myös aliorganisaatioiden sisällyttäminen") @QueryParam("s") Boolean subOrganizations,
-            @ApiParam("Myös organisaatiottomat henkilöt") @QueryParam("no") Boolean noOrganizations,
-            @ApiParam("Myös suljetut käyttöoikeudet") @QueryParam("groupClosed") Boolean groupClosed,
-            @ApiParam("Rajaus huoltajan hetun tai OID:n mukaan") @QueryParam("guardian") String guardian,
-            @ApiParam("Myös duplikaattihenkilöiden haku") @QueryParam("d") Boolean duplicates);
 
     @GET
     @Produces(JSON)
