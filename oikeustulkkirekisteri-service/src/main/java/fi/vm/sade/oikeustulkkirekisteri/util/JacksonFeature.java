16ad424d466a0075029cf559fc4726ecc0c4379a
Jani Kaarela <jani.kaarela@gmail.com>
2020-10-30 13:53:14 +0200

    Korvattu Guava JDK8:n ominaisuuksilla.

--- oikeustulkkirekisteri-service/src/main/java/fi/vm/sade/oikeustulkkirekisteri/util/JacksonFeature.java
+++ oikeustulkkirekisteri-service/src/main/java/fi/vm/sade/oikeustulkkirekisteri/util/JacksonFeature.java
@@ -3,6 +3,7 @@ package fi.vm.sade.oikeustulkkirekisteri.util;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
@@ -12,6 +13,7 @@ public class JacksonFeature extends JacksonJaxbJsonProvider {
         setMapper(new ObjectMapper() {
             private static final long serialVersionUID = 1L;
             {
+                registerModule(new Jdk8Module());
                 registerModule(new JavaTimeModule());
                 // True
                 enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
